7c9729dea15e214fce3c6a34526f4bc195de6f91
Oleksii Kletsov <akletsov88@gmail.com>
2025-12-09 20:40:11 -0500

    fixing sql that returns finished events

--- src/main/resources/sql/get_no_result_events.sql
+++ src/main/resources/sql/get_no_result_events.sql
@@ -1,6 +1,6 @@
 SELECT
 	id
 from event e
-where e.date_scheduled < now() + INTERVAL 2 hour
+where e.date_scheduled < now() - INTERVAL 3 hour
 	and e.`result` is null
 order by e.date_scheduled desc;
